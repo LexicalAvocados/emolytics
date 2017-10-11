d84b3aa781d4138836ec9f7874c0b64e754032a4
Lucas Ashby <lfeashby@gmail.com>
2017-10-11 16:59:04 -0400

    Revised db model requires for user, section and project

--- server/router.js
+++ server/router.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const router = require('express').Router();
-<<<<<<< HEAD
 const testerRoutes = require('./api/tester');
 
 
 
 router.use('/tester', testerRoutes);
-=======
-const projectRoutes = require('./api/creator/projectRoutes.js')
-
-
-router.get('/getRelatedSections', projectRoutes.getRelatedSection);
->>>>>>> Basic setup for request to server for all sections from projectHome complete
 
 module.exports = router;
