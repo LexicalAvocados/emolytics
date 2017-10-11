0c541e2a1840cfca70fc8bfbb2311cd9eb9ce898
ANR <anr@Adityas-MacBook-Pro.local>
2017-10-11 19:06:15 -0400

    basic navbar complete

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, ButtonToolbar, ButtonGroup } from 'react-bootstrap';
 
 const Navbar = (props) => {
 
@@ -8,29 +7,25 @@ const Navbar = (props) => {
     <div>
     { !props.isLoggedIn ?
       (
-        <div classID='NavOutside' style={navbarStyle}>
-          <ButtonToolbar>
-            <Link to='/login'>
-              <Button> Log in </Button>
-            </Link>
-            <Link to='/signup'>
-              <Button> Sign up </Button>
-            </Link>
-          </ButtonToolbar>
+        <div className='navContainer' style={navbarContainerStyle}>
+          <Link to='/login' style={noUnderline}>
+            <p className='navItem'>Log in </p>
+          </Link>
+          <Link to='/signup' style={noUnderline}>
+            <p className='navItem'>Sign up</p>
+          </Link>
         </div>
       ) : (
-          <div classID='NavInside' style={navbarStyle}>
-          <ButtonToolbar>
-            <Link to='/'>
-              <Button bsStyle="primary">Home</Button>
+          <div className='navContainer' style={navbarContainerStyle}>
+            <Link to='/' style={noUnderline}>
+              <p className='navItem'>Home</p>
             </Link>
-            <Link to='/projects'>
-              <Button>Projects</Button>
+            <Link to='/projects' style={noUnderline}>
+              <p className='navItem'>Projects</p>
             </Link>
-            <Link to='/logout'>
-              <Button>Log out</Button>
+            <Link to='/logout' style={noUnderline}>
+              <p className='navItem'>Log out</p>
             </Link>
-          </ButtonToolbar>
         </div>
       )
     }
@@ -38,8 +33,15 @@ const Navbar = (props) => {
   )
 };
 
-const navbarStyle = {
-  float: "right"
+const navbarContainerStyle = {
+  float: "right",
+  display: "flex",
+  padding: "10px",
+  marginRight: "3%"
+}
+
+const noUnderline = {
+  textDecoration: "none"
 }
 
 export default Navbar;
