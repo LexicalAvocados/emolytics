9e0612853a920d1a1dc6e1c7a5fc7436c4239726
Lucas Ashby <lfeashby@gmail.com>
2017-10-11 16:36:04 -0400

    Commit before rebase

--- server/api/creator/projectRoutes.js
+++ server/api/creator/projectRoutes.js
@@ -1,6 +1,8 @@
 // Import relevant things from db 
+const sections = require('../../../db/models/section.js')
 
 exports.getRelatedSection = function(req, res) {
+  // send projectId, grab sections, use sectionid to grab options
   console.log(req.query);
   res.send('here we are');
 };
