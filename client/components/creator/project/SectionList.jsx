b32c98a176789540890684cf2d2ba039ee02b9bf
Lucas Ashby <lfeashby@gmail.com>
2017-10-11 15:28:24 -0400

    Basic setup for projectHome

---
+++ client/components/creator/project/SectionList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SectionList = (props) => (
+  <div>
+    <p>Name of the section</p>
+    {/* VideoThumbnails with the section - map returning separate entities*/}
+  </div>
+)
