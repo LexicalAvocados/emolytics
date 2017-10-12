02d48469839326f021594a4a5acfc947ec981059
Craig Gordon <cyghfer@gmail.com>
2017-10-11 22:09:20 -0400

    Add initial backend auth logic

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const Bluebird = require('bluebird');
 const router = require('./server/router')
 const db = require('./db');
+const signup = require('./server/auth/signup.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +16,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/client'));
 
 app.use('/api', router)
+
+app.use(session({
+  secret: 'machine learning',
+  cookie: {
+    maxAge: 60000,
+  }
+}));
+
+app.get('/signup', )
 
 app.get('*', (req, res) => {
 	res.sendFile(__dirname + '/client/index.html');
