16715af429c7e4d544023ab759f926af43adf282
YuChao Dong <yuchaodong@gmail.com>
2017-10-12 15:47:17 -0400

    resolving conflicts

--- server/router.js
+++ server/router.js
@@ -10,23 +10,5 @@ router.get('/getRelatedSections', projectRoutes.getRelatedSection);
 
 router.get('/getOptionsForSection', sectionRoutes.getRelatedOptions);
 
-// router.post('/createProject', projectRoutes.createProjectHandler);
-
-// router.post('/createProject', projectRoutes.createProject);
-
-// router.post('/createProject', projectRoutes.createProject);
-
-// router.get('/', function(req, res) {
-//   console.log('testing');
-//   res.send('testing');
-// });
-
-// router.post('/', function(req, res) {
-//   console.log('testing post');
-//   res.send('testing post endpoint');
-// });
-// router.post('/createProject', function(req, res) {
-//   res.send('createProject');
-// });
 
 module.exports = router;
