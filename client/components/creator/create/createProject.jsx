5dbe331ccdc548eab881e95b1662da172df4be2e
YuChao Dong <yuchaodong@gmail.com>
2017-10-13 14:16:17 -0400

    working on createProject frontend

--- client/components/creator/create/createProject.jsx
+++ client/components/creator/create/createProject.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
-
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as ChangeActions from '../../../actions';
