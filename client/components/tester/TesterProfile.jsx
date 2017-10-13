46dce790213ae332428cdd502e6870f4d75569a8
Craig Gordon <cyghfer@gmail.com>
2017-10-13 19:45:28 -0400

    Add logic for editing Sex and Race on User Profile

--- client/components/tester/TesterProfile.jsx
+++ client/components/tester/TesterProfile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, FormGroup, FormControl, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Form, FormGroup, FormControl, ListGroup, ListGroupItem, Option, ButtonToolbar, ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
 import axios from 'axios';
 
 // React-Redux connect() boilerplate
@@ -18,7 +18,13 @@ class TesterProfile extends React.Component {
     }
     this.startEditingAge = this.startEditingAge.bind(this);
     this.stopEditingAge = this.stopEditingAge.bind(this);
+    this.startEditingSex = this.startEditingSex.bind(this);
+    this.stopEditingSex = this.stopEditingSex.bind(this);
+    this.startEditingRace = this.startEditingRace.bind(this);
+    this.stopEditingRace = this.stopEditingRace.bind(this);
     this.updateAge = this.updateAge.bind(this);
+    this.updateSex = this.updateSex.bind(this);
+    this.updateRace = this.updateRace.bind(this);
   }
 
   startEditingAge() {
@@ -28,6 +34,22 @@ class TesterProfile extends React.Component {
   stopEditingAge(e) {
     e.preventDefault();
     this.setState({editingAge: false});
+  }
+
+  startEditingSex() {
+    this.setState({editingSex: true});
+  }
+
+  stopEditingSex(e) {
+    this.setState({editingSex: false});
+  }
+
+  startEditingRace() {
+    this.setState({editingRace: true});
+  }
+
+  stopEditingRace(e) {
+    this.setState({editingRace: false});
   }
 
   updateAge(e) {
@@ -40,23 +62,61 @@ class TesterProfile extends React.Component {
     }
   }
 
+  updateSex(e) {
+    this.props.actions.setSex(e);
+  }
+
+  updateRace(e) {
+    this.props.actions.setRace(e);
+  }
+
   render() {
     return (
       <div>
-        <form onSubmit={this.stopEditingAge}>
-          <FormGroup>
-            <ListGroup>
-              <ListGroupItem onClick={this.startEditingAge}>
-                Age: {this.state.editingAge ? 
+        <ListGroup>
+
+          <ListGroupItem onClick={this.startEditingAge}>
+            Age: {this.state.editingAge ?
+              <form onSubmit={this.stopEditingAge}>
+                <FormGroup>
                   <FormControl type='text' value={this.props.loggedInUser.age} onChange={this.updateAge} />
-                :
-                  <span>{this.props.loggedInUser.age}</span>}
-              </ListGroupItem>
-              <ListGroupItem>Sex</ListGroupItem>
-              <ListGroupItem>Race</ListGroupItem>
-            </ListGroup>
-          </FormGroup>
-        </form>
+                </FormGroup>
+              </form>
+            :
+              <span>{this.props.loggedInUser.age}</span>}
+          </ListGroupItem>
+
+          <ListGroupItem onClick={this.startEditingSex}>
+            Sex: {this.state.editingSex ?
+              <ButtonToolbar>
+                <ToggleButtonGroup type='radio' name='sexOptions' onChange={(e) => {this.updateSex(e); this.stopEditingSex(e)}}>
+                  <ToggleButton value='Male'>Male</ToggleButton>
+                  <ToggleButton value='Female'>Female</ToggleButton>
+                </ToggleButtonGroup>
+              </ButtonToolbar>
+            :
+              <span>{this.props.loggedInUser.sex}</span>
+            }
+          </ListGroupItem>
+
+          <ListGroupItem onClick={this.startEditingRace}>
+            Race: {this.state.editingRace ?
+              <ButtonToolbar>
+                <ToggleButtonGroup type='radio' name='raceOptions' onChange={(e) => {this.updateRace(e); this.stopEditingRace(e)}}>
+                  <ToggleButton value='Caucasian'>Caucasian</ToggleButton>
+                  <ToggleButton value='Hispanic'>Hispanic</ToggleButton>
+                  <ToggleButton value='African American'>African American</ToggleButton>
+                  <ToggleButton value='Asian'>Asian</ToggleButton>
+                  <ToggleButton value='Pacific Islander'>Pacific Islander</ToggleButton>
+                  <ToggleButton value='Native American'>Native American</ToggleButton>
+                  <ToggleButton value='Other'>Other Ethnicity</ToggleButton>
+                </ToggleButtonGroup>
+              </ButtonToolbar>
+            :
+              <span>{this.props.loggedInUser.race}</span>}
+          </ListGroupItem>
+
+        </ListGroup>
       </div>
     )
   }
