93afdfae04d1fd37b18f93370e8827a2cb50b17d
Max Peng <pengcheng95@gmail.com>
2017-10-13 16:03:12 -0400

    fixed req.session

--- client/components/tester/testerVideo.jsx
+++ client/components/tester/testerVideo.jsx
@@ -20,7 +20,7 @@ class TesterVideo extends React.Component {
         name: '',
         desc: ''
       },
-      complete: false
+      complete: false,
       time: [0],
       img: []
       
@@ -30,7 +30,7 @@ class TesterVideo extends React.Component {
     this.takePicture = this.takePicture.bind(this);
     this.checkVideo = this.checkVideo.bind(this);
     this.processImage = this.processImage.bind(this);
-    this.upload = this.upload.bind(this);
+    // this.upload = this.upload.bind(this);
   }
 
   componentDidMount() {
@@ -121,26 +121,25 @@ class TesterVideo extends React.Component {
   checkVideo() {
     var video = this.refs.video;
     var time = Math.floor(video.getCurrentTime());
-    console.log(time);
     if (!this.state.time.includes(time)) {
       console.log(time);
       this.takePicture();
       this.state.time.push(time);
-      this.processImage();
+      this.processImage(time);
     }
 
   }
 
-  upload(img) {
-    cloudinary.config({
-      cloud_name: 'dcp74bsm8',
-      api_key: '169847461734926',
-      api_secret: 'Gqk2mNBC1D8q3LJQe0b1fLokn-Y'
-    });
-    cloudinary.uploader.upload(
-      img,
-      function(res) {
-        console.log('res from cloudinary', res)
+  // upload(img) {
+  //   cloudinary.config({
+  //     cloud_name: 'dcp74bsm8',
+  //     api_key: '169847461734926',
+  //     api_secret: 'Gqk2mNBC1D8q3LJQe0b1fLokn-Y'
+  //   });
+  //   cloudinary.uploader.upload(
+  //     img,
+  //     function(res) {
+  //       console.log('res from cloudinary', res)
         // Microsoft Post Request
         // var subscriptionKey = "4fc26d1500d04025a699f1ae74597ab3";
         // var uriBase = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?";
@@ -203,57 +202,120 @@ class TesterVideo extends React.Component {
         // });
 
 
-      }
-    )
-  }
+  //     }
+  //   )
+  // }
 
-  processImage() {
+  processImage(time) {
 
 
         var canvas = this.refs.canvas;
         // DOM element data Uri
         var data = canvas.toDataURL('image/jpeg');
 
-        this.upload(data);
-
         // pure base64 data
-        var realData = data.replace(/^data:image\/(png|jpg);base64,/, "")
+        // var realData = data.replace(/^data:image\/(png|jpg);base64,/, "")
         
-        // blob data
-        var blobData = this.state.img[0];
+        // // blob data
+        // var blobData = this.state.img[0];
 
-        // file format
-        var file = new File([blobData], 'test');
+        // // file format
+        // var file = new File([blobData], 'test');
 
-        // form format
-        var fd = new FormData();
-        fd.append('data', blobData);
+        // // form format
+        // var fd = new FormData();
+        // fd.append('data', blobData);
 
-        console.log('file format', file);
-        console.log('blob format', blobData);
-        console.log('form data', fd);
+        // console.log('file format', file);
+        // console.log('blob format', blobData);
+        // console.log('form data', fd);
 
-        // test to see if image about to send is the correct one
-        var test = this.refs.test;
-        test.setAttribute('src', data);
+        // // test to see if image about to send is the correct one
+        // var test = this.refs.test;
+        // test.setAttribute('src', data);
 
-        var dataTest = data.split(',')[1];
-        var mimeType = data.split(';')[0].slice(5);
+        // var dataTest = data.split(',')[1];
+        // var mimeType = data.split(';')[0].slice(5);
 
-        var bytes = window.atob(dataTest);
-        var buf = new ArrayBuffer(bytes.length);
-        var byteArr = new Uint8Array(buf);
+        // var bytes = window.atob(dataTest);
+        // var buf = new ArrayBuffer(bytes.length);
+        // var byteArr = new Uint8Array(buf);
 
-        for (var i = 0; i < bytes.length; i++) {
-          byteArr[i] = bytes.charCodeAt(i);
-        }
+        // for (var i = 0; i < bytes.length; i++) {
+        //   byteArr[i] = bytes.charCodeAt(i);
+        // }
+        fetch(data) 
+          .then(res => res.blob())
+          .then(blobData => {
+            // Microsoft Post Request
+            var subscriptionKey = "4fc26d1500d04025a699f1ae74597ab3";
+            var uriBase = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?";
+            // var uriBase = "https://requestb.in/1ijcwry1";
+
+            // axios.get(uriBase);
+
+            // axios.post(uriBase, {data: byteArr});
+            // var instance = axios.create({
+            //   headers: {
+            //     "Content-Type" : "application/octet-stream"
+            //   }
+            // });
+            // instance.post('https://requestb.in/wpray0wp', byteArr);
+
+            // var url ='https://requestb.in/zdz1bczd'
+            // request(url, function (error, response, body) {
+            //   if (!error) {
+            //     console.log(body);
+            //   }
+            // });
+
+            // Request parameters.
+            var params = {
+              "returnFaceId": "true",
+              "returnFaceLandmarks": "false",
+              "returnFaceAttributes": "emotion",
+            };
+
+            $.ajax({
+                // url: uriBase + "?" + $.param(params),
+                url: uriBase,
+
+                // Request headers.
+                beforeSend: function(xhrObj){
+                    xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+                    // xhrObj.setRequestHeader("Access-Control-Allow-Origin", "*");
+                    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+                },
+
+                type: "POST",
+
+                // Request body.
+                data: blobData,
+                processData: false,
+            })
+
+            .done(function(data) {
+                // Show formatted JSON on webpage.
+                console.log((JSON.stringify(data, null, 2)));
+
+                axios.post('/api/tester/sendFrame', data[0].scores)
+                  .then(res => {
+                    console.log(res);
+                  })
+            })
+
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                // Display error message.
+                var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+                errorString += (jqXHR.responseText === "") ? "" : (jQuery.parseJSON(jqXHR.responseText).message) ? 
+                    jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
+                alert(errorString);
+            });
+          });
 
         //var dataBlob = canvas.toBlob()
-
-
-        console.log('byteArr', byteArr);
-
-        this.state.img[0] = byteArr;
+        // console.log('byteArr', byteArr);
+        // this.state.img[0] = byteArr;
 
 
 
@@ -282,66 +344,7 @@ class TesterVideo extends React.Component {
         //   .then(res => console.log(res))
 
 
-        // Microsoft Post Request
-        var subscriptionKey = "4fc26d1500d04025a699f1ae74597ab3";
-        var uriBase = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?";
-        // var uriBase = "https://requestb.in/1ijcwry1";
-
-        // axios.get(uriBase);
-
-        // axios.post(uriBase, {data: byteArr});
-        // var instance = axios.create({
-        //   headers: {
-        //     "Content-Type" : "application/octet-stream"
-        //   }
-        // });
-        // instance.post('https://requestb.in/wpray0wp', byteArr);
-
-        // var url ='https://requestb.in/zdz1bczd'
-        // request(url, function (error, response, body) {
-        //   if (!error) {
-        //     console.log(body);
-        //   }
-        // });
-
-        // Request parameters.
-        var params = {
-          "returnFaceId": "true",
-          "returnFaceLandmarks": "false",
-          "returnFaceAttributes": "emotion",
-        };
-
-        $.ajax({
-            // url: uriBase + "?" + $.param(params),
-            url: uriBase,
-
-            // Request headers.
-            beforeSend: function(xhrObj){
-                xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
-                // xhrObj.setRequestHeader("Access-Control-Allow-Origin", "*");
-                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-            },
-
-            type: "POST",
-
-            // Request body.
-            data: blobData,
-            processData: false,
-        })
-
-        .done(function(data) {
-            // Show formatted JSON on webpage.
-            console.log('data', data)
-            console.log((JSON.stringify(data, null, 2)));
-        })
-
-        .fail(function(jqXHR, textStatus, errorThrown) {
-            // Display error message.
-            var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
-            errorString += (jqXHR.responseText === "") ? "" : (jQuery.parseJSON(jqXHR.responseText).message) ? 
-                jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
-            alert(errorString);
-        });
+       
 
       // let s3Upload = new S3Upload(AWS, {
       //   Bucket: 'reaction-sync',
@@ -376,16 +379,15 @@ class TesterVideo extends React.Component {
         <h2> {this.state.video.name} </h2>
         <h4> {this.state.video.desc} </h4>
         <div class="camera">
-          <video id="video">Video stream not available.</video>
-          <button onClick={this.takePicture} id="startbutton">Take photo</button>
+          <video style={imgStyle} id="video">Video stream not available.</video>
         </div>
       
 
         <canvas style={imgStyle} ref="canvas" id="canvas">
         </canvas>
         <div class="output">
-          <img ref="photo" id="photo" alt="The screen capture will appear in this box."/>
-          <img ref="test" alt="The screen capture will appear in this box."/>
+          <img style={imgStyle} ref="photo" id="photo" alt="The screen capture will appear in this box."/>
+          <img style={imgStyle} ref="test" alt="The screen capture will appear in this box."/>
         </div>
       </div>
 
