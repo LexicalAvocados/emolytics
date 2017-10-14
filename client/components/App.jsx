bfb840ae7b92269ae8961d35d627428dc159be4e
Craig Gordon <cyghfer@gmail.com>
2017-10-14 16:10:32 -0400

    Fix Login/Signup redirect + fix loggedInUser Redux property

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, withRouter } from 'react-router-dom'
 import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
@@ -94,7 +94,7 @@ const mapDispatchToProps = (dispatch) => ({
   actions: bindActionCreators(ChangeActions, dispatch)
 });
 
-export default connect(
+export default withRouter(connect(
   mapStateToProps,
   mapDispatchToProps
-)(App);
+)(App));
