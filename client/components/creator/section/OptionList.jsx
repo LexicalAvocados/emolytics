f881bfda972905dba6d308127a589bcd09409201
Lucas Ashby <lfeashby@gmail.com>
2017-10-16 12:32:39 -0400

    Removed comments

--- client/components/creator/section/OptionList.jsx
+++ client/components/creator/section/OptionList.jsx
@@ -41,28 +41,3 @@ class OptionList extends React.Component {
 }
 
 export default OptionList;
-
-
-// Separate component because we will want to display more information and style it easily
-// class Invited extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-
-
-
-//   render () {
-//     return (
-//       <div>
-//         <p>Invited:</p>
-//         {this.props.invited.map((invitee, i) => (
-//           <p key={i}>Name: {invitee.username} Email: {invitee.email}</p>
-//         ))}
-//         <button onClick={this.props.submitInvites}>Finish Inviting...</button>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Invited;
