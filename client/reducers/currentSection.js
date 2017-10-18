953c05aeb5de22b1804ce6ed10250a4960f7354a
Lucas Ashby <lfeashby@gmail.com>
2017-10-18 17:36:35 -0400

    Converted duration from api request to seconds, fixed input to global store in addOption

--- client/reducers/currentSection.js
+++ client/reducers/currentSection.js
@@ -1,10 +1,12 @@
-const currentSection = (state = {}, action) => {
+const currentSection = (state = { options: [] }, action) => {
   switch (action.type) {
     case 'CHANGE_CURRENT_SECTION':
       return Object.assign({}, state, action.obj);
     case 'CHANGE_CURRENT_OPTION':
       // return Object.assign({}, state, action.optionObj);
       return { ...state, option: action.optionObj }
+    case 'ADD_OPTIONS_TO_CURRENT_SECTION':
+      return { ...state, options: [ ...state.options, action.optionObj]};
     default:
       return state;
   }
