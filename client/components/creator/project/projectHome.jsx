decda75a9a8425ed3e0fdee5bcacff411a36e822
YuChao Dong <yuchaodong@gmail.com>
2017-10-25 15:44:56 -0400

    componentWillUnmount

--- client/components/creator/project/projectHome.jsx
+++ client/components/creator/project/projectHome.jsx
@@ -21,6 +21,7 @@ class ProjectHome extends React.Component {
   }
 
   componentWillUnmount() {
+
   }
 
   onSectionClick(obj, options) {
@@ -44,7 +45,7 @@ class ProjectHome extends React.Component {
     return (
       <div className="projectHomeContainer">
         <Collapse in={this.state.open}>
-          <Panel header={`Project Title: ${this.props.currentProject.name}`} bsStyle="warning">
+          <Panel header={`Project Name: ${this.props.currentProject.name}`} bsStyle="warning">
             <h6>Description: {this.props.currentProject.description}</h6>
           </Panel>
         </Collapse>
